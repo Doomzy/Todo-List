8275d2a852c6fb93d28e7b6ad3e166a47d006e79
Doomzy <mohamed_abdulhamid5@outlook.com>
2021-05-07 02:15:22 +0200

    finished the

--- todoApp/views.py
+++ todoApp/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import response, HttpResponse
 from rest_framework.views import APIView
+from .models import Task
+from .serializer import TaskSerializer
 
 class TodoView(APIView):
 
+    def getTask(self, request):
+        modifId= request.POST.get("id")
+        return Task.objects.get(id= modifId)
+
     def get(self, request):
-        return render(request, 'tdTemp.html')
+        query_set_todo= Task.objects.filter(isItDone=False)
+        query_set_done= Task.objects.filter(isItDone=True)
+        return render(request, 'tdTemp.html', 
+            {"tasks_todo":query_set_todo, "tasks_done":query_set_done}
+        )
+
+    def post(self, request):
+            modifMethod= request.POST.get("_method")
+            if modifMethod == "add":
+                serialized = TaskSerializer(data=request.data)  
+                if serialized.is_valid():
+                    serialized.save()
+                    return redirect('/', status=200)
+    
+            elif modifMethod == "done":
+                wantedTask= self.getTask(request)
+                wantedTask.isItDone= True
+                wantedTask.save()
+                return redirect('/', status=200)
+
+            elif modifMethod == "delete":
+                wantedTask= self.getTask(request)
+                wantedTask.delete()
+                return redirect('/', status=200)
+            
+            return HttpResponse( status=400)
+    
